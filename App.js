085044b7c87d7c5b6928ee9e1cb1a6446aa99b3f
laphon khodchawat <khodchawatguide@gmail.com>
2025-07-20 16:25:07 +0700

    first commit

---
+++ App.js
@@ -0,0 +1,20 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Laphon Khodchawat DBT.102</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#6495ED',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
